a18a5c8ebb9322a40231eeea32d2838b97e893c0
Aaron Durant <aaron@aarondurant.com>
2022-05-09 16:04:40 -0400

    Add buttons to projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -8,7 +8,6 @@ const projectsList = [
   {
     name: 'Personal Website',
     description: 'Built with Gatsby and hosted on Netlify. This version of aarondurant.com marks a significant step forward from the personal websites I made on WordPress over the last several years but never really updated.',
-    words: 'https://www.aarondurant.com/hello-world/',
     code: 'personal-website',
     demo: 'https://www.aarondurant.com',
     emoji: '🤘🏻',
@@ -17,7 +16,6 @@ const projectsList = [
   {
     name: 'ARMA Sport Concept',
     description: 'In putting my spin on the website of nutritional supplement brand ARMA Sport, I fortified my knowledge of React and CSS, including Flexbox, Grid, the BEM methodology, media queries, and more.',
-    words: 'https://www.aarondurant.com/hello-world/',
     code: 'arma-concept',
     demo: 'https://arma-concept.netlify.app',
     emoji: '💪🏻',
@@ -26,7 +24,6 @@ const projectsList = [
   {
     name: 'Pipe Dream Blog',
     description: 'Given my background in writing about racing, ceaseless thoughts about writing about motocross, and my deep interest in static websites paired with a headless CMS, it was only a matter of time before Pipe Dream hit a server near you.',
-    words: 'https://pipe-dream.netlify.app/',
     code: 'pipe-dream',
     demo: 'https://pipe-dream.netlify.app/',
     emoji: '🏁',
@@ -35,7 +32,6 @@ const projectsList = [
   {
     name: 'Responsive Portfolio',
     description: 'A modern portfolio website using HTML, CSS and JavaScript. Here, I demonstrate a few basic skills and display what I worked on when I was first learning to code via CareerFoundry.',
-    words: 'https://www.aarondurant.com/hello-world/',
     code: 'simple-portfolio',
     demo: 'https://aaronrdurant.github.io/simple-portfolio/',
     emoji: '👨🏻‍💻',
@@ -68,6 +64,24 @@ const Projects = ({ data, location }) => {
               <div>
                 <h2 className="name">{project.name}</h2>
                 <p className="description">{project.description}</p>
+                <div className="buttons">
+                  <a
+                    href={`https://github.com/AaronRDurant/${project.code}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Code
+                  </a>
+                  {project.demo && (
+                    <a
+                      href={project.demo}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Demo
+                    </a>
+                  )}
+                </div>
               </div>
             </div>
           ))}
